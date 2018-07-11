4eb831b728794cfa3496d4dbbb42e6cf7c7e76a8
Misho Tsenteradze <mtsenteradze@gmail.com>
2018-07-11 17:47:42 +0400

    add button added

---
+++ toDo/ToDoListViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  toDo
+//
+//  Created by Mac on 11.07.18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["work", "study", "house"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+  
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        let addItemAlert = UIAlertController(title: "add item", message: nil, preferredStyle: .alert)
+        addItemAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        addItemAlert.addTextField { (textField) in
+            textField.text = "sdd list item"
+        }
+        addItemAlert.addAction(UIAlertAction(title: "add", style: .default, handler: { (action) in
+            if let item = addItemAlert.textFields?.first?.text{
+                print(item)
+            }
+        }))
+        self.present(addItemAlert, animated: true)
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
